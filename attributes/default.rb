f4accc7fcfdc3a85d80cc032ecd408a6698e2a2f
Ed Fisher <ed@mrskin.com>
2014-07-28 15:19:17 -0500

    fixes for non-suse

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,8 +26,8 @@ when 'suse'
     xorg-x11-xauth xorg-x11-fonts
   )
 else
-  Chef::Application.fatal! "FIXME: #{node.platform} platform not supported yet"
-  return
+  default[:pacemaker][:platform][:packages] = %w(pacemaker crmsh)
+  default[:pacemaker][:platform][:graphical_packages] = %w(pacemaker-mgmt pacemaker-mgmt-client)
 end
 
 default[:pacemaker][:founder] = false
